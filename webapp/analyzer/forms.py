eb99f422b0951f28078ac363700de76c59f4e637
jacobBuhler <jacob.buhler@icloud.com>
2026-04-23 11:33:04 -0700

    added style to the page and dark theme consistent throughout

--- webapp/analyzer/forms.py
+++ webapp/analyzer/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from .models import AnalysisJob
+
+
+def _apply_bootstrap_classes(fields):
+    #attach bootstrap widget class
+    for field in fields.values():
+        widget = field.widget
+        existing = widget.attrs.get('class', '')
+
+        if isinstance(widget, forms.Select):
+            css = 'form-select'
+        elif isinstance(widget, forms.CheckboxInput):
+            css = 'form-check-input'
+        elif isinstance(widget, forms.ClearableFileInput) or isinstance(widget, forms.FileInput):
+            css = 'form-control'
+        else:
+            css = 'form-control'
+
+        widget.attrs['class'] = (existing + ' ' + css).strip()
 
 
 class SimpleSignupForm(forms.ModelForm):
@@ -10,6 +29,10 @@ class SimpleSignupForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        _apply_bootstrap_classes(self.fields)
 
     def clean(self):
         cleaned_data = super().clean()
@@ -26,3 +49,14 @@ class AnalysisJobForm(forms.ModelForm):
     class Meta:
         model = AnalysisJob
         fields = ['title', 'paradigm', 'uploaded_file', 'threads', 'runs', 'extra_args']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        _apply_bootstrap_classes(self.fields)
+
+
+class BootstrapLoginForm(AuthenticationForm):
+    #login form that wears bootstrap wrapper
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        _apply_bootstrap_classes(self.fields)
--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from analyzer.forms import BootstrapLoginForm
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'accounts/login/',
+        LoginView.as_view(authentication_form=BootstrapLoginForm),
+        name='login',
+    ),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('analyzer.urls')),
 ]
